51cdcf2ae0178af2b0ae36908a41e36922315d31
Grahame Bowland <grahame@angrygoats.net>
2016-05-28 18:11:16 +0800

    load.sh improvements

--- load.sh
+++ load.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /scratch
+
 if [ ! -d "2011 Datapacks BCP_IP_TSP_PEP_ECP_WPP_ERP_Release 3" ]; then
     if [ ! -f '2011 Datapacks BCP_IP_TSP_PEP_ECP_WPP_ERP_Release 3.7z' ]; then
         wget -O datapacks.tmp https://dl.dropboxusercontent.com/u/10671102/census/2011%20Datapacks%20BCP_IP_TSP_PEP_ECP_WPP_ERP_Release%203.7z &&
@@ -9,11 +11,11 @@ if [ ! -d "2011 Datapacks BCP_IP_TSP_PEP_ECP_WPP_ERP_Release 3" ]; then
 fi
 
 echo "loading the 2011 Australian Census"
-ealgis set projected_srid 3112 && 
-ealgis set map_srid 3857 && 
+ealgis set projected_srid 3112 &&
+ealgis set map_srid 3857 &&
 ealgis set map_default_lat -27.121915157767 &&
 ealgis set map_default_lon 133.21253738715 &&
 ealgis set map_default_zoom 4 &&
-ealgis run aus_census_2011.py
-echo 'VACUUM ANALYZE;' | /app/develop.sh psql
 
+ealgis run /recipes/ealgis-aus-census-2011/aus_census_2011.py
+echo 'VACUUM ANALYZE;' | /app/develop.sh psql
